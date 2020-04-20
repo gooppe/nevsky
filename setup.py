9c2c0ec48aa6fa57ee937dda6af5c38e5d07f35e
Alexander Danilevsky <danilevskii.aleksandr97@gmail.com>
2020-04-20 18:34:50 +0300

    requirements and setup modified

--- setup.py
+++ setup.py
@@ -5,7 +5,13 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     description="Simple and lightweighted translation bot",
-    python_requires=">=3.5.0",
-    install_requires=["torch>=1.4.0", "youtokentome>=1.0.6", "tqdm", "numpy"],
+    python_requires=">=3.6.0",
+    install_requires=[
+        "torch==1.4.0",
+        "youtokentome>=1.0.6",
+        "tqdm",
+        "numpy",
+        "pyTelegramBotAPI",
+    ],
     entry_points={"console_scripts": ["nevsky = nevsky.cli:main"]},
 )
